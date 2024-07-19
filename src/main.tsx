9177a2779d2e4db294901a38aa7ea41c1ecf2985
ahui <782492184@qq.com>
2024-07-19 14:41:59 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,3 @@
-// import './patch-local-storage-for-github-pages';
-
-// import { StrictMode } from 'react'
-// import { render } from 'react-dom';
-// import App from './App'
-// import './index.scss'
-// // 开发调试需要用到
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
-// import { ConfigProvider } from 'antd-mobile';
-// import enUS from 'antd-mobile/es/locales/en-US'
-// import eruda from "eruda";
-
-// eruda.init();
-
-// render(
-//     <StrictMode>
-//         <ConfigProvider locale={enUS}>
-//             <Provider store={store}>
-//                 <App />
-//             </Provider>
-//         </ConfigProvider>
-//     </StrictMode>,
-//     document.getElementById('root') as HTMLElement
-// )
-
 import ReactDOM from 'react-dom/client';
 
 import { Root } from '@/components/Root';
@@ -35,9 +9,6 @@ import './mockEnv';
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.scss';
 
-// import eruda from "eruda";
-
-// eruda.init();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<Root />);
 

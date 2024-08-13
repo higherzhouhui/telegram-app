0e0574fd9c6c34cae925d019cc17a76a782d2ec2
ahui <782492184@qq.com>
2024-08-13 13:48:13 +0800

    update

--- src/constants/config/login.config.mainnet.ts
+++ src/constants/config/login.config.mainnet.ts
@@ -1,7 +1,7 @@
 import { NetworkEnum, TChainId } from "@aelf-web-login/wallet-adapter-base";
 
 export const CHAIN_ID = "tDVV" satisfies TChainId;
-export const TELEGRAM_BOT_ID = '7329439364';
+export const TELEGRAM_BOT_ID = '7186075668';
 export const NETWORK_TYPE = NetworkEnum.MAINNET;
 export const RPC_SERVER_AELF = "https://aelf-test-node.aelf.io";
 export const RPC_SERVER_TDVV = "https://tdvv-test-node.aelf.io";

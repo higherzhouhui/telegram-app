253d2aa1579e5097b4b51c5d05fc852e68a85232
ahui <782492184@qq.com>
2024-08-17 23:26:14 +0800

    update

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,43 +3,18 @@ import {
   SDKProvider
 } from '@telegram-apps/sdk-react';
 
-import { lazy, Suspense, useEffect, useState, type FC } from 'react';
+import { lazy, Suspense, type FC } from 'react';
 import Loading from '@/components/Loading';
 
 const TgApp = lazy(() => import('./Tg'))
-const PcApp = lazy(() => import('./Pc'))
-
 
 export const App: FC = () => {
-  // 判断当前环境
-  const [isTgMini, setIsTgMini] = useState(true)
-  const currentTest: string = 'TG'
-  useEffect(() => {
-    // 本地开发特殊处理
-    if (location.href.includes('localhost')) {
-      if (currentTest == 'TG') {
-        setIsTgMini(true)
-      } else {
-        setIsTgMini(false)
-      }
-    } else {
-      // 逻辑重写1。先判断是否为tg小程序
-      // const search = location.search
-      // if (search.includes('tgWebApp')) {
-      //   setIsTgMini(true)
-      // } else {
-      //   setIsTgMini(false)
-      // }
-    }
-  }, [])
+
   return (
     <Suspense fallback={<Loading />}>
-      {
-        isTgMini ? <SDKProvider>
-          <TgApp />
-        </SDKProvider> : <PcApp />
-      }
-
+      <SDKProvider>
+        <TgApp />
+      </SDKProvider>
     </Suspense>
 
   );

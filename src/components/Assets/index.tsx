7d0f63f5b6fa4c2eeb7a80b56de3d4570713ec2b
ahui <782492184@qq.com>
2024-08-19 15:58:58 +0800

    update

---
+++ src/components/Assets/index.tsx
@@ -0,0 +1,53 @@
+import { useCallback, useEffect, useMemo } from 'react';
+import { LeftOutlined } from '@ant-design/icons';
+import { WalletTypeEnum } from '@aelf-web-login/wallet-adapter-base';
+import { useConnectWallet } from '@aelf-web-login/wallet-adapter-react';
+import { PortkeyDid } from '@aelf-web-login/wallet-adapter-bridge';
+import { ExtraInfoForPortkeyAA } from './type';
+import { useNavigate } from 'react-router-dom';
+
+export default function MyAsset() {
+  const navigate = useNavigate()
+  const { walletType, walletInfo } = useConnectWallet();
+  const portkeyAAInfo = useMemo(() => {
+    return walletInfo?.extraInfo as ExtraInfoForPortkeyAA;
+  }, [walletInfo?.extraInfo]);
+
+  const handleDeleteAccount = useCallback(() => {
+    localStorage.clear();
+  }, []);
+
+  useEffect(() => {
+    if (walletType !== WalletTypeEnum.aa) {
+      navigate('/')
+    }
+  }, [walletType, navigate]);
+
+  if (
+    walletType !== WalletTypeEnum.aa ||
+    !portkeyAAInfo?.portkeyInfo?.pin ||
+    !portkeyAAInfo?.portkeyInfo?.chainId
+  ) {
+    return null;
+  }
+
+  return (
+    <div className={styles['my-asset-wrapper']}>
+      <PortkeyDid.PortkeyAssetProvider
+        originChainId={portkeyAAInfo?.portkeyInfo?.chainId}
+        pin={portkeyAAInfo?.portkeyInfo?.pin}>
+        <PortkeyDid.Asset
+          isShowRamp={false}
+          isShowRampBuy={false}
+          isShowRampSell={false}
+          backIcon={<LeftOutlined />}
+          onOverviewBack={() => navigate(-1)}
+          onLifeCycleChange={(lifeCycle) => {
+            console.log(lifeCycle, 'onLifeCycleChange');
+          }}
+          onDeleteAccount={handleDeleteAccount}
+        />
+      </PortkeyDid.PortkeyAssetProvider>
+    </div>
+  );
+}
--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -5,6 +5,7 @@ import GamePage from '@/pages/Game';
 import TaskPage from '@/pages/Task';
 import FrensPage from '@/pages/Frens';
 import WalletPage from '@/pages/Wallet';
+import AssetsPage from '@/components/Assets';
 import LeaderBoardPage from '@/pages/LeaderBoard';
 import FrensDetailPage from '@/pages/Frens/Detail';
 
@@ -24,4 +25,5 @@ export const routes: Route[] = [
   { path: '/leaderboard', Component: LeaderBoardPage },
   { path: '/wallet', Component: WalletPage },
   { path: '/frens-detail', Component: FrensDetailPage },
+  { path: '/assets', Component: AssetsPage },
 ];

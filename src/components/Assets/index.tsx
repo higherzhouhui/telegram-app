07c924bc3cdf0944ff49e917984acb9ba527842a
ahui <782492184@qq.com>
2024-08-21 16:11:20 +0800

    update

--- src/components/Assets/index.tsx
+++ src/components/Assets/index.tsx
@@ -1,14 +1,29 @@
-import { useCallback, useEffect, useMemo } from 'react';
-import { LeftOutlined } from '@ant-design/icons';
-import { WalletTypeEnum } from '@aelf-web-login/wallet-adapter-base';
-import { useConnectWallet } from '@aelf-web-login/wallet-adapter-react';
-import { PortkeyDid } from '@aelf-web-login/wallet-adapter-bridge';
-import { ExtraInfoForPortkeyAA } from './type';
-import { useNavigate } from 'react-router-dom';
-import './index.scss'
-export default function MyAsset() {
-  const navigate = useNavigate()
+import { useCallback, useEffect, useMemo } from "react";
+import { LeftOutlined } from "@ant-design/icons";
+import { WalletTypeEnum } from "@aelf-web-login/wallet-adapter-base";
+import { useConnectWallet } from "@aelf-web-login/wallet-adapter-react";
+import { PortkeyDid } from "@aelf-web-login/wallet-adapter-bridge";
+import { useNavigate } from "react-router-dom";
+import "./index.scss";
+
+export interface ExtraInfoForPortkeyAA {
+  publicKey: string;
+  portkeyInfo: PortkeyDid.DIDWalletInfo & {
+    accounts: TAelfAccounts;
+    nickName: string;
+  };
+}
+
+export type TAelfAccounts = {
+  AELF?: string;
+  tDVV?: string;
+  tDVW?: string;
+};
+
+export default function Assets() {
   const { walletType, walletInfo } = useConnectWallet();
+
+  const navigate = useNavigate();
   const portkeyAAInfo = useMemo(() => {
     return walletInfo?.extraInfo as ExtraInfoForPortkeyAA;
   }, [walletInfo?.extraInfo]);
@@ -18,10 +33,11 @@ export default function MyAsset() {
   }, []);
 
   useEffect(() => {
-    console.log(walletType)
-    console.log(walletInfo)
+    console.log('walletType', walletType)
+    console.log('walletInfo', walletInfo)
+    console.log('portkeyAAInfo', portkeyAAInfo)
     if (walletType !== WalletTypeEnum.aa) {
-      navigate('/')
+      // navigate("/");
     }
   }, [walletType, navigate]);
 
@@ -30,26 +46,28 @@ export default function MyAsset() {
     !portkeyAAInfo?.portkeyInfo?.pin ||
     !portkeyAAInfo?.portkeyInfo?.chainId
   ) {
-    return null;
+    return 'null';
   }
 
   return (
-    <div className='my-asset-wrapper'>
-      <PortkeyDid.PortkeyAssetProvider
-        originChainId={portkeyAAInfo?.portkeyInfo?.chainId}
-        pin={portkeyAAInfo?.portkeyInfo?.pin}>
-        <PortkeyDid.Asset
-          isShowRamp={false}
-          isShowRampBuy={false}
-          isShowRampSell={false}
-          backIcon={<LeftOutlined />}
-          onOverviewBack={() => navigate(-1)}
-          onLifeCycleChange={(lifeCycle) => {
-            console.log(lifeCycle, 'onLifeCycleChange');
-          }}
-          onDeleteAccount={handleDeleteAccount}
-        />
-      </PortkeyDid.PortkeyAssetProvider>
+    <div className={"my-asset-wrapper"}>
+      {portkeyAAInfo?.portkeyInfo?.pin && (
+        <PortkeyDid.PortkeyAssetProvider
+          originChainId={portkeyAAInfo?.portkeyInfo?.chainId}
+          pin={portkeyAAInfo?.portkeyInfo?.pin}>
+          <PortkeyDid.Asset
+            isShowRamp={false}
+            isShowRampBuy={false}
+            isShowRampSell={false}
+            backIcon={<LeftOutlined />}
+            onOverviewBack={() => navigate("/")}
+            onLifeCycleChange={(lifeCycle) => {
+              console.log(lifeCycle, "onLifeCycleChange");
+            }}
+            onDeleteAccount={handleDeleteAccount}
+          />
+        </PortkeyDid.PortkeyAssetProvider>
+      )}
     </div>
   );
 }

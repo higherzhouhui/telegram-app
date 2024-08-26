9b39fb02c840420901a8132997fa1d5465ef3479
ahui <782492184@qq.com>
2024-08-26 14:53:59 +0800

    fix bug

--- src/components/App/Pc.tsx
+++ src/components/App/Pc.tsx
@@ -76,6 +76,7 @@ const PcApp: FC = () => {
     }
   }
   useEffect(() => {
+    localStorage.setItem('h5PcRoot', '1')
     // initApp()
     const onMessage = ({ visible, time }: { visible: boolean, time?: number }) => {
       setShowCongrates(visible)
@@ -94,19 +95,20 @@ const PcApp: FC = () => {
 
 
   return (
-    <div className='layout maxWidth' id='layout'>
+    <div className='layout maxWidth pcLayout' id='layout'>
+      <Footer isH5PcRoot />
+      {
+        isShowBack ? <header onClick={() => navigate(-1)}>
+          <svg className='backBtn' viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2332" width="24" height="24"><path d="M395.21518 513.604544l323.135538-312.373427c19.052938-18.416442 19.052938-48.273447 0-66.660212-19.053961-18.416442-49.910737-18.416442-68.964698 0L291.75176 480.290811c-19.052938 18.416442-19.052938 48.273447 0 66.660212l357.633237 345.688183c9.525957 9.207709 22.01234 13.796214 34.497699 13.796214 12.485359 0 24.971741-4.588505 34.466999-13.82896 19.052938-18.416442 19.052938-48.242747 0-66.660212L395.21518 513.604544z" fill="#ffffff" p-id="2333"></path></svg>
+        </header> : null
+      }
       <div className='content'>
-        {
-          isShowBack ? <header onClick={() => navigate(-1)}>
-            <svg className='backBtn' onClick={() => navigate(-1)} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2332" width="24" height="24"><path d="M395.21518 513.604544l323.135538-312.373427c19.052938-18.416442 19.052938-48.273447 0-66.660212-19.053961-18.416442-49.910737-18.416442-68.964698 0L291.75176 480.290811c-19.052938 18.416442-19.052938 48.273447 0 66.660212l357.633237 345.688183c9.525957 9.207709 22.01234 13.796214 34.497699 13.796214 12.485359 0 24.971741-4.588505 34.466999-13.82896 19.052938-18.416442 19.052938-48.242747 0-66.660212L395.21518 513.604544z" fill="#ffffff" p-id="2333"></path></svg>
-          </header> : null
-        }
         <Routes>
           {routes.map((route) => <Route key={route.path} {...route} />)}
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
+        <div className='bot' style={{ height: '20px' }} />
       </div>
-      <Footer />
       <Congrates visible={isShowCongrates} time={showTime} callBack={() => setShowCongrates(false)} />
       {
         loading ? <Loading /> : null

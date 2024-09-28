e47b214aea8856e0f1e9fa94c48624fa09f2c89a
sunnyjqs <sunnyjqshi180@nddn.io>
2024-09-28 18:27:49 +0800

    fix: 🐛 scan url

--- src/constants/config/login.config.testnet.ts
+++ src/constants/config/login.config.testnet.ts
@@ -8,7 +8,7 @@ export const RPC_SERVER_AELF = "https://aelf-test-node.aelf.io";
 export const RPC_SERVER_TDVV = "https://tdvv-public-node.aelf.io";
 export const RPC_SERVER_TDVW = "https://tdvw-test-node.aelf.io";
 export const GRAPHQL_SERVER =
-  "https://dapp-aa-portkey-test.portkey.finance/Portkey_DID/PortKeyIndexerCASchema/graphql";
+  "https://dapp-aa-portkey-test.portkey.finance/Portkey_V2_DID/PortKeyIndexerCASchema/graphql";
 
 export const CONNECT_SERVER = "https://auth-aa-portkey-test.portkey.finance";
 

8f68e4f9b6b34e9eaee63e2a3cae77612148a5a7
sunnyjqs <sunnyjqshi180@nddn.io>
2024-10-11 13:15:41 +0800

    change networktype

--- src/constants/config/login.config.mainnet.ts
+++ src/constants/config/login.config.mainnet.ts
@@ -1,9 +1,9 @@
-import { TChainId } from "@aelf-web-login/wallet-adapter-base";
+import { NetworkEnum,TChainId } from "@aelf-web-login/wallet-adapter-base";
 
 export const CHAIN_ID = "tDVV" satisfies TChainId;
 export const TELEGRAM_BOT_ID = import.meta.env.VITE_TELEGRAM_BOT_ID;
 
-export const NETWORK_TYPE = import.meta.env.VITE_NETWORK_TYPE;
+export const NETWORK_TYPE = NetworkEnum.MAINNET;
 export const RPC_SERVER_AELF = "https://explorer-test.aelf.io/chain";
 export const RPC_SERVER_TDVV = "https://explorer-test-side02.aelf.io/chain";
 export const RPC_SERVER_TDVW = "https://explorer-test-side02.aelf.io/chain";

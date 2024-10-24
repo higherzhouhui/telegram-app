dd4453a8ba330095f191263a7d87f6f8bf94111e
ahui <782492184@qq.com>
2024-10-24 11:25:17 +0800

    update

--- src/pages/Airdrop/index.tsx
+++ src/pages/Airdrop/index.tsx
@@ -57,7 +57,7 @@ function AirDropPage() {
 
   const handleReset = () => {
     localStorage.clear()
-    setChoseItem({})
+    setWithDrawInfo({})
   }
 
   const handleOption = (index: number) => {
@@ -138,9 +138,8 @@ function AirDropPage() {
 
   return <div className='airdrop-page'>
     {/* <div className='title'>Airdrop</div> */}
-    <div className='title'>Snapshot</div>
 
-    <div className='time'>Until October 24, 2024. 12:00 UTC</div>
+    <div className='time'>October 24, 2024. 18:00 UTC</div>
     <div className='have'>
       <div className='have-title'>You have</div>
       <div className='balance'>{userInfo?.score?.toLocaleString()} $HMSTR</div>
